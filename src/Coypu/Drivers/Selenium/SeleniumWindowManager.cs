80289eff5b2af937908df0ad473cd8d2ad257c1b
Marcel Holle <marcel.holle@rubicon.eu>
2020-03-17 11:46:20 +0100

    #1 Fix Firefox failing to switch out of iframe

--- src/Coypu/Drivers/Selenium/SeleniumWindowManager.cs
+++ src/Coypu/Drivers/Selenium/SeleniumWindowManager.cs
@@ -1,4 +1,27 @@
-﻿using OpenQA.Selenium;
+﻿// MIT License
+//
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// Copyright (c) 2018 Adrian Longley & Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
 namespace Coypu.Drivers.Selenium
@@ -34,18 +57,17 @@ namespace Coypu.Drivers.Selenium
 
         public void SwitchToWindow(string windowName)
         {
-            // Fix for https://bugzilla.mozilla.org/show_bug.cgi?id=1305822 
-            var isFirefox = _webDriver is FirefoxDriver;
-
-            if (LastKnownWindowHandle != windowName || !isFirefox && SwitchedToAFrame)
+            if (LastKnownWindowHandle != windowName || SwitchedToAFrame)
             {
                 _webDriver.SwitchTo().Window(windowName);
-                LastKnownWindowHandle = windowName;
-            }
 
-            if (isFirefox && SwitchedToAFrame)
-            {
-                _webDriver.SwitchTo().DefaultContent();
+                // Fix for https://bugzilla.mozilla.org/show_bug.cgi?id=1305822
+                if (_webDriver is FirefoxDriver)
+                {
+                    _webDriver.SwitchTo().DefaultContent();
+                }
+
+                LastKnownWindowHandle = windowName;
             }
 
             _switchedToFrame = null;
